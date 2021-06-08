a60a3a5d3534c7aae54009de86df69ddf02d643b
wanghui62 <wanghui62@ZBMAC-C02DW5LS1.local>
2021-06-08 21:02:53 +0800

    add evil

--- init.el
+++ init.el
@@ -31,5 +31,6 @@
 (require 'init-magit)
 (require 'init-youdao)
 (require 'init-python)
+(require 'init-evil)
 
 (put 'downcase-region 'disabled nil)
---
+++ lisp/init-evil.el
@@ -0,0 +1,16 @@
+(require-package 'evil)
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+(require-package 'evil-collection)
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(provide 'init-evil)
